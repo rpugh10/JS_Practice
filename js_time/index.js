bad0b8d4866cffca6d3b24f8b2e3c51dfdaa48e7
Ryan Pugh <rpugh777@yahoo.com>
2026-06-12 22:10:00 +0800

    Files for learning JS

---
+++ js_time/index.js
@@ -0,0 +1,44 @@
+/**
+ * ! Logging to console
+ */
+
+
+//console.log('Hello World');
+//console.log('The current date and time is: ' + new Date());
+
+/**
+ * ! Alerts
+ */
+
+//window.alert('The current date and time is: ' + new Date());
+
+/**
+ * ! Document Manipulation
+ */
+
+//document.getElementById('myH1').textContent = 'hello';
+//document.getElementById('myP').textContent = 'goodbye';
+
+/**
+ * ! Variables and Data Types
+ */
+
+//let age = 25;
+
+//let price = 10.99;
+//let gpa = 3.5;
+
+//console.log('The amount is $' + price);
+
+//console.log(typeof price);
+
+//let name = 'Ryan';
+
+//document.getElementById('myH1').textContent = 'My name is ' + name;
+
+//console.log('My name is ' + name);
+
+//let isStudent = true;
+
+//console.log('Is the person a student? ' + isStudent);
+
