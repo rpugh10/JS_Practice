5f5d17b5e598d64aab5333dd7431998392b3a02e
Ryan Pugh <rpugh777@yahoo.com>
2026-06-15 23:48:17 -0700

    JS notes/pratice

--- js_time/index.js
+++ js_time/index.js
@@ -2,43 +2,89 @@
  * ! Logging to console
  */
 
+/**
+ *? console.log('Hello World');
+ *? console.log('The current date and time is: ' + new Date());
+*/
 
-//console.log('Hello World');
-//console.log('The current date and time is: ' + new Date());
 
 /**
  * ! Alerts
  */
 
-//window.alert('The current date and time is: ' + new Date());
+/**
+ *? window.alert('The current date and time is: ' + new Date());
+*/
+
 
 /**
  * ! Document Manipulation
  */
 
-//document.getElementById('myH1').textContent = 'hello';
-//document.getElementById('myP').textContent = 'goodbye';
+/**
+ *? document.getElementById('myH1').textContent = 'hello';
+ *? document.getElementById('myP').textContent = 'goodbye';
+*/
+
 
 /**
  * ! Variables and Data Types
  */
 
-//let age = 25;
+/**
+ *? let age = 25;
+ *? let gpa = 3.5;
+ *? console.log('The amount is $' + price);
+ *? console.log(typeof price);
+ *? let name = 'Ryan';
+ *? document.getElementById('myH1').textContent = 'My name is ' + name;
+ *? console.log('My name is ' + name);
+*/
 
-//let price = 10.99;
-//let gpa = 3.5;
+// ?const PI = 3.14; // Constant variable cannot be changed 
 
-//console.log('The amount is $' + price);
+/**
+ * ! User Input
+ */
 
-//console.log(typeof price);
+//Easy Way Window PROMPT
+/**
+ *? let username = window.prompt('What is your name?');
+ *? console.log('Hello, ' + username + '!');
+*/
 
-//let name = 'Ryan';
+//Hard Way HTML TextBox
+/*
+* Note: Need to create an HTML input element and a button 
+* to trigger the event. Then, use JavaScript to get the value from the input and display it.
+*/
 
-//document.getElementById('myH1').textContent = 'My name is ' + name;
+/**
+ * ?document.getElementById('submitBtn').onclick = function() {
+    *? let username = document.getElementById('username').value;
+    *? console.log('Hello, ' + username + '!');
+}
+*/
 
-//console.log('My name is ' + name);
+/* 
+? let username = 'Ryan';
+? document.getElementById('header').textContent = 'Welcome to JS' + username;
+ */
 
-//let isStudent = true;
+/* 
+! Type Conversion 
+*/
 
-//console.log('Is the person a student? ' + isStudent);
+/* let age = window.prompt('How old are you?');
+? age = Number(age); // Convert string to number
+? age += 1; // Increment age by 1
+? console.log('Next year, you will be ' + age + ' years old.');
+ */
+
+/* 
+? let x = 'pizza';
+
+? x = Number(x);
+? console.log(x, typeof x); 
+*/
 
