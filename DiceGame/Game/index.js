fbd9c202f2234026676f3a8386ea4f4174e7cd80
Ryan Pugh <rpugh777@yahoo.com>
2026-07-17 18:27:11 -0700

    Completed Dice Game

--- DiceGame/Game/index.js
+++ DiceGame/Game/index.js
@@ -1,3 +1,19 @@
 function rollDice(){
-    
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="../dice_img/${value}.png">`);
+    }
+
+    diceResult.textContent = `You rolled: ${values.join(", ")}`;
+    diceImages.innerHTML = images.join("");
+    console.log(images);
+
+    //innerHTMl takes the images and puts them in the div with the id of diceImages
 }
